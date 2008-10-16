7510a6c97c001989d683c1e322c134f85dc3d11a
syuu <syuu@0baba885-a8eb-41f5-96ae-9b54537fed86>
2008-10-16 16:33:49 +0000

    git-svn-id: https://enoki.dokukino.com/svn/public/shiitake@261 0baba885-a8eb-41f5-96ae-9b54537fed86

---
+++ trunk/process.c
@@ -0,0 +1,25 @@
+#include <types.h>
+#include <cpu.h>
+#include <process.h>
+#include <segment.h>
+
+#define PROCESS_TABLE_SIZE 1024
+
+static struct process process_table[PROCESS_TABLE_SIZE] = {{0}};
+
+static inline void
+tr_load(uint16_t selector)
+{
+	asm volatile("ltr %0" : : "r" (selector));
+}
+
+void process_init(void)
+{
+	/* set process 0 descriptor */
+	gdt_set_tss(0x100, &process_table[0], TSS_TYPE_NORMAL,
+		    TSS_PRIVILEGE_LEVEL_KERNEL, TSS_PRESENT,
+		    TSS_GRANULARITY_ENABLE);
+
+	tr_load(0x100);
+}
+
