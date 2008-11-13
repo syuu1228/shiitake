fd357ba18e9930ddaeb0a82b7a88d96df2a3b95f
syuu <syuu@0baba885-a8eb-41f5-96ae-9b54537fed86>
2008-11-13 09:05:52 +0000

    git-svn-id: https://enoki.dokukino.com/svn/public/shiitake@285 0baba885-a8eb-41f5-96ae-9b54537fed86

---
+++ trunk/kernel/kern/elf.h
@@ -0,0 +1,55 @@
+#ifndef KERN_ELF_H_
+#define KERN_ELF_H_
+#include <lib/types.h>
+
+typedef struct {
+	unsigned char e_ident[16];
+	uint16_t e_type;
+	uint16_t e_machine;
+	uint32_t e_version;
+	uint32_t e_entry;
+	uint32_t e_phoff;
+	uint32_t e_shoff;
+	uint32_t e_flags;
+	uint16_t e_ehsize;
+	uint16_t e_phentsize;
+	uint16_t e_phnum;
+	uint16_t e_shentsize;
+	uint16_t e_shnum;
+	uint16_t e_shstrndx;
+} elf32_header_t;
+
+#define EI_MAG0    0
+#define EI_MAG1    1
+#define EI_MAG2    2
+#define EI_MAG3    3
+#define EI_CLASS   4
+#define EI_DATA    5
+#define EI_VERSION 6
+#define EI_OSABI   7
+
+typedef struct {
+	uint32_t p_type;
+	uint32_t p_offset;
+	uint32_t p_vaddr;
+	uint32_t p_paddr;
+	uint32_t p_filesz;
+	uint32_t p_memsz;
+	uint32_t p_flags;
+	uint32_t p_align;
+} elf32_program_header_t;
+
+typedef struct {
+	uint32_t sh_name;
+	uint32_t sh_type;
+	uint32_t sh_flags;
+	uint32_t sh_addr;
+	uint32_t sh_offset;
+	uint32_t sh_size;
+	uint32_t sh_link;
+	uint32_t sh_info;
+	uint32_t sh_addralign;
+	uint32_t sh_entsize;
+} elf32_section_header_t;
+
+#endif
