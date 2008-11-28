189e0b71aec0786d118a2699c9597c8798cb67d3
syuu <syuu@0baba885-a8eb-41f5-96ae-9b54537fed86>
2008-11-28 17:02:13 +0000

    git-svn-id: https://enoki.dokukino.com/svn/public/shiitake@291 0baba885-a8eb-41f5-96ae-9b54537fed86

--- trunk/kernel/mips/syscall.c
+++ trunk/kernel/mips/syscall.c
@@ -1,6 +1,7 @@
 #include <mips/cpu.h>
 #include <kern/syscall.h>
 #include <mips/syscall.h>
+#include <lib/console.h>
 
 #define DPRINTF (printf("[%s:%s:%d] ", __FILE__, __FUNCTION__, __LINE__), printf)
 //#define DPRINTF(...) do{}while(0)
@@ -11,9 +12,10 @@ void syscall_handle(unsigned status, unsigned cause, unsigned epc,
 	DPRINTF("syscall code:%x arg0:%x arg1:%x arg2:%x arg3:%x\n",
 		frame->v0, frame->a0, frame->a1, frame->a2, frame->a3);
 
-	if(cause & CAUSE_BD)
+	if(cause & CAUSE_BD) {
 		panic("delay slot is not supported in syscall\n");
-	else
+	}else{
 		frame->pc = epc + sizeof(int);
+	}
 	syscall_invoke(frame);
 }
